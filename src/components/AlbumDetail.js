6e86fa759817d9b63bd8ce529e039341a96f9bb9
Taylor Mock <tmock12@gmail.com>
2018-04-05 18:12:50 +0300

    Create card header

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, Image, View } from "react-native";
+import Card from "./Card";
+import CardSection from "./CardSection";
 
 const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
-    <View>
-      <Text>
-        {album.title}
-      </Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={styles.thumbnailContainer}>
+          <Image style={styles.thumbnail} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={styles.cardHeaderTextContainer}>
+          <Text style={styles.cardHeaderText}>
+            {title}
+          </Text>
+          <Text>
+            {artist}
+          </Text>
+        </View>
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  thumbnail: {
+    width: 50,
+    height: 50
+  },
+  thumbnailContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  cardHeaderTextContainer: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  cardHeaderText: {
+    fontSize: 18
+  }
 };
 
 export default AlbumDetail;
