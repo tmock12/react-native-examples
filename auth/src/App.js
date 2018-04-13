7b7a15ee81e94d5a60dd090909b65f0d91c99e40
Taylor Mock <tmock12@gmail.com>
2018-04-13 12:27:13 +0300

    User signs out

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,19 +2,50 @@ import React, { Component } from 'react';
 import { SecretKeys } from './secretKeys';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import {
+  Card,
+  CardSection,
+  Header,
+  Button,
+  Spinner,
+} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentDidMount() {
     firebase.initializeApp(SecretKeys.firebaseSecrets);
+
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ loggedIn: !!user });
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button
+            onPress={() => {
+              firebase.auth().signOut();
+            }}
+          >
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
